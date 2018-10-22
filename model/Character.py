dc6ca8bde91450e4f96abf13e7611b5e0868233c
WhiteKittens <quinten.possemiers@gmail.com>
2018-10-22 14:19:19 +0200

    Interface weapon part 1

--- model/Character.py
+++ model/Character.py
@@ -14,13 +14,13 @@ class Character:
         self.character_name = character_name
         self.character_inventory = list()
         self.character_full_stats = dict()
-        self.equiped_items = []
+        self.equipped_items = []
         self.init_equipment_slots()
         self.init_stats()
 
     def init_equipment_slots(self):
         for equipment_slot in range(self.AMOUNT_OF_EQUIPMENT_SLOTS):
-            self.equiped_items += [None]
+            self.equipped_items += [None]
 
     def add_to_inventory(self, item):
         self.character_inventory.append(item)
@@ -30,14 +30,17 @@ class Character:
 
     def equip_item(self, item):
         if item.get_equipment_type in WeaponType.get_two_handed_weapons():
-            if self.equiped_items[1] is not None:
-                self.character_inventory.append(self.equiped_items[1])
-                self.equiped_items[1] = None
+            if self.equipped_items[1] is not None:
+                self.character_inventory.append(self.equipped_items[1])
+                self.equipped_items[1] = None
 
-        if self.equiped_items[item.get_equipment_type().value[3]] is not None:
-            self.character_inventory.append(self.equiped_items[item.get_equipment_type().value[3]])
-        self.equiped_items[item.get_equipment_type().value[3]] = item
+        if self.equipped_items[item.get_equipment_type().value[3]] is not None:
+            self.character_inventory.append(self.equipped_items[item.get_equipment_type().value[3]])
+        self.equipped_items[item.get_equipment_type().value[3]] = item
         self.recalculate_stats()
+
+    def de_equip_item(self, item):
+        self.equipped_items[item.get_equipment_type().value[3]] = None
 
     def init_stats(self):
         for stat in list(Stats):
@@ -46,15 +49,23 @@ class Character:
             self.character_full_stats[attribute] = 0
 
     def recalculate_stats(self):
-        for item in self.equiped_items:
+        for item in self.equipped_items:
             if item is not None:
                 for stat in item.get_stats_and_attr():
                     self.character_full_stats[stat] += item.get_stats_and_attr()[stat][1]
+        return
+
+    def print_character_stats(self):
+        for stat in self.character_full_stats:
+            if 0 < self.character_full_stats[stat]:
+                print("%-28s  : %4d" % (stat.name.replace("_", " "), self.character_full_stats[stat]))
+        return
+
+
 
 
 weapon = Weapon(False)
 character = Character("my char")
 character.add_to_inventory(weapon)
 character.equip_item(weapon)
-print(character.character_full_stats)
-print(WeaponType.two_handed_sword)
+character.print_character_stats()
